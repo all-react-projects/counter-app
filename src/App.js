bdedf2d5b14438ca323a1ac8b1a73f2909988633
hiteshwarkaim <hemant.kaimwal96@gmail.com>
2023-05-09 22:07:04 +0530

    counter app finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count,setCount]=useState(0);
+
+  const handleIncrease=()=>{
+    setCount(count+1)
+  }
+
+  const handleDecrease=()=>{
+    if(count===0)
+      alert('Negative number is starting')
+    setCount(count-1);
+  }
+
+  const handleReset=()=>{
+    setCount(0);
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <button className='inc' onClick={handleDecrease}>-</button>
+        <div className='num'>{count}</div>
+        <button className='dec' onClick={handleIncrease}>+</button>
+      </div>
+      <button id='reset' onClick={handleReset}>Reset</button>
+      
     </div>
   );
 }
